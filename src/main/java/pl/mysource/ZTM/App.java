9079797f7710621f82d28b067783b85ecc393a7c
Damian Muszyński <d.muszynski112@gmail.com>
2018-10-23 11:15:46 +0200

    message

--- src/main/java/pl/mysource/ZTM/App.java
+++ src/main/java/pl/mysource/ZTM/App.java
@@ -17,6 +17,7 @@ public class App {
         javax.swing.SwingUtilities.invokeLater(new Runnable(){
             public void run(){
                 createAndShowGUI();
+                System.out.println("cos");
             }
 
             });
