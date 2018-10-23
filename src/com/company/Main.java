0e1bcb4351b208dab4d02efdd9400b1f8934e8a5
Mateusz <mgodlewski25@gmail.com>
2018-10-23 11:08:59 +0200

    0.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.awt.EventQueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Window();
+            }
+        });
+
+    }
+}
